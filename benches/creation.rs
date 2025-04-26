0e318866073567ba7e12523e73186b9180ccd0de
Quaternions <krakow20@gmail.com>
2025-04-26 02:55:04 -0700

    test hash_str global and local

--- benches/creation.rs
+++ benches/creation.rs
@@ -4,11 +4,12 @@ use criterion::black_box;
 use criterion::Criterion;
 use crossbeam_channel::bounded;
 use crossbeam_utils::thread::scope;
+use hash_str::HashStr;
 use std::sync::Arc;
 use string_cache::DefaultAtom;
 use string_interner::StringInterner;
 
-use ustr::*;
+use ustr::ustr;
 
 use parking_lot::Mutex;
 
@@ -38,6 +39,29 @@ fn criterion_benchmark(c: &mut Criterion) {
             unsafe { ustr::_clear_cache() };
             for s in s.iter().cycle().take(100_000) {
                 black_box(ustr(s));
+            }
+        });
+    });
+
+    let hash_str_cache = hash_str::get_cache();
+
+    let s = raft.clone();
+    c.bench_function("single raft hash_str global", move |b| {
+        b.iter(|| {
+            unsafe { hash_str::_clear_cache() };
+            for s in s.iter().cycle().take(100_000) {
+                black_box(hash_str_cache.intern_str(s));
+            }
+        });
+    });
+
+    let s = raft.clone();
+    c.bench_function("single raft hash_str local", move |b| {
+        b.iter(|| {
+            let host = hash_str::HashStrHost::new();
+            let mut cache = hash_str::HashStrCache::new();
+            for s in s.iter().cycle().take(100_000) {
+                black_box(cache.intern_str_with(&host, s));
             }
         });
     });
